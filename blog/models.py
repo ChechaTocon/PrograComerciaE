c9becc0ae9502ba6c9dd11e3b4c2b6e762c01c05
ChechaTocon <cesarchecha91@gmail.com>
2020-11-15 14:22:06 -0600

    test aceptados de forma local

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 
 class Publicacion(models.Model):
     autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    titulo = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=100)
     texto = models.TextField()#la diferencia es que aqui le pone un text área.
     fecha_creacion = models.DateTimeField('Creado',#etiqueta para ponerle al momento de crearse
             default=timezone.now)
